55d0799bb0a2ab0b4f95d887829c8fd35fa9cead
Sun He <sunheehnus@gmail.com>
2014-04-19 15:38:48 +0800

    autologin.sh: login automatically

---
+++ autologin.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# author: sunheehnus
+# NEEDS SUPER USER PRIVILEGE
+# para1: username (default autologin user)
+# para2: cnt (tty's cnt. user autologin tty${cnt})
+#
+# Usage:
+# 	sudo ./autologin.sh sunheehnus 1
+# 	sunheehnus will autologin on tty1
+
+cd /usr/bin
+touch autologin
+chmod +x autologin
+echo "#!/bin/bash
+/bin/login -f $1" > autologin
+
+cd /etc/init/
+stable=`grep -v exec tty${2}.conf`
+change=`grep exec tty${2}.conf`
+
+echo "$stable" >"tty${2}.conf"
+echo "$change"| while read oneline
+do
+	[[ $oneline == "#"* ]] && echo "$oneline"
+	[[ $oneline == "#"* ]] || echo "# $oneline"
+done >>"tty${2}.conf"
+echo "exec /sbin/getty -n -1 /usr/bin/autologin -8 38400 tty${2}" >>"tty${2}.conf"
+
+echo "After reboot it will work."
